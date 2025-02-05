18d5f6ab6ae901659863c5b0d7ccba4c0c5a6ca5
Abhisp2711 <abhisp2711@gmail.com>
2025-02-05 20:25:41 +0530

    added new song

--- JS/script.js
+++ JS/script.js
@@ -8,13 +8,35 @@ const move = document.querySelector(".fa-music");
 const audio = document.getElementById("audio");
 const progressContainer = document.getElementById("progress-container");
 const progress = document.getElementById("progress");
-
 // Play Music
 function playSong() {
     playIcon.classList.replace("fa-play", "fa-pause");
     audio.play();
     rotator.classList.add("rotating");  // Start rotating
     move.classList.add("move");  // Start rotating
+}
+//songs list
+songs = ['aaj_ki_rat',
+    'Bano_ki_saheli',
+    'bole_chudiyaa',
+    'chhote_chhote_bhaiyoke',
+    'Dholak_me_tak',
+    'Dholak_me_tak',
+    'doli_saja_ke',
+    'ghumaro',
+    'lal_dupata',
+    'payal_hai_chhankai',
+    'tut_gayi_chudiya',
+];
+
+//get song index 
+let songIndex = 0;
+
+//load song details with audio src
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+    audio.src = `/Music/${song}.mp3`;
 }
 
 // Pause Music
@@ -23,6 +45,25 @@ function pauseSong() {
     audio.pause();
     rotator.classList.remove("rotating");  // Stop rotating
     move.classList.remove("move");  // Stop rotating
+}
+
+// next song
+function playNextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+// prev song
+function playPrevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
 }
 
 // Toggle Play/Pause
@@ -50,11 +91,8 @@ progressContainer.addEventListener("click", (e) => {
     audio.currentTime = (clickX / width) * duration;
 });
 
-// OPTIONAL: Next & Previous Button Functionality (If you add a playlist)
-prevBtn.addEventListener("click", () => {
-    audio.currentTime -= 10; // Rewind 10 seconds
-});
+nextBtn.addEventListener('click', playNextSong);
 
-nextBtn.addEventListener("click", () => {
-    audio.currentTime += 10; // Forward 10 seconds
-});
+prevBtn.addEventListener('click', playPrevSong);
+
+
