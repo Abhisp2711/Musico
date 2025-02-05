f60bd181da858d8f86a1ad2f0e0d0b9eace64b2a
Abhisp2711 <abhisp2711@gmail.com>
2025-02-05 14:23:44 +0530

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+const container = document.getElementById('container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+
+console.log(playBtn);
